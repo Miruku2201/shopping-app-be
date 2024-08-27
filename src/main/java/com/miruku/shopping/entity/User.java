ec8c8679f8464bf4a03b16f5268830f385862ded
Miruku2201 <miruku2201@gmail.com>
2024-08-27 09:58:32 +0700

    Starting Project

---
+++ src/main/java/com/miruku/shopping/entity/User.java
@@ -0,0 +1,36 @@
+package com.miruku.shopping.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private int roles;
+
+    // Thông tin cá nhân người dùng.
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+}
